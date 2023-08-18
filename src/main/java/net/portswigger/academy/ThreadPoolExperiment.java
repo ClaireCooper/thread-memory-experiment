236c7eb80dd20f01a5f3db680e412c11176f09e4
claire.cooper <claire.cooper@portswigger.net>
2023-08-18 14:20:47 +0100

    ACAD-1614 Change to java 17 not 11

--- src/main/java/net/portswigger/academy/ThreadPoolExperiment.java
+++ src/main/java/net/portswigger/academy/ThreadPoolExperiment.java
@@ -6,8 +6,8 @@ import java.util.concurrent.*;
 public class ThreadPoolExperiment {
     public static void main(String[] args) throws InterruptedException {
         int threadPoolSize = 1024;
-        int processesToRun = 2048;
-        int megabytesToPutInHeap = 550;
+        int processesToRun = 2*1024;
+        int megabytesToPutInHeap = 650;
 
         System.out.println("Test started...");
         byte[] heapFiller = new byte[megabytesToPutInHeap*1024*1024];
@@ -22,13 +22,28 @@ public class ThreadPoolExperiment {
             }
             return true;
         };
+        Callable<Boolean> threadStackMediumTask = () -> {
+            halfFillStack(0L);
+            return true;
+        };
+        Callable<Boolean> threadStackMediumishTask = () -> {
+            partFillStack(0L);
+            return true;
+        };
+        Callable<Boolean> threadStackLightTask = () -> {
+            doNothing(0);
+            return true;
+        };
+
 
         System.out.println("Starting thread execution");
-        ExecutorService executor = Executors.newFixedThreadPool(threadPoolSize);
-
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(threadPoolSize);
+        System.out.println(executor.getActiveCount() + " active threads in pool");
+//        Thread.sleep(10000);
         List<Future<Boolean>> outcomes = new ArrayList<>();
 
         try {
+
             for (int i = 1; i <= processesToRun; i++) {
                 outcomes.add(executor.submit(threadStackFillerTask));
                 System.out.println("Submitted: " + i);
@@ -39,17 +54,22 @@ public class ThreadPoolExperiment {
 //                happily replace closed threads' stacks)
                 Thread.sleep(10);
             }
-            for (int i = 0; i < processesToRun; i++) {
-                System.out.println(i + 1 + " " + outcomes.get(i).get());
+
+            for (Future<Boolean> o:outcomes
+                 ) {
+                System.out.println(outcomes.indexOf(o)+1 + " " + o.get());
             }
-//            Thread.sleep(50000);
+            System.out.println(executor.getActiveCount() + " active threads in pool");
+            Thread.sleep(10000);
         }
         catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
         finally {
 //            executor.shutdownNow();
+            System.out.println(executor.getActiveCount() + " active threads in pool");//0 active but thread stacks still there
             executor.shutdown();
+            System.out.println("Shutdown threads");
         }
     }
 
@@ -63,4 +83,28 @@ public class ThreadPoolExperiment {
         return fillStack(n+1);
     }
 
+    static private int partFillStack(long n) throws InterruptedException {
+        if (n==3000)
+        {
+            System.out.println("Part filled stack in " + Thread.currentThread().getName());
+//            Thread.sleep(30*1000);
+            return 0;
+        }
+        return partFillStack(n+1);
+    }
+
+    static private int halfFillStack(long n) throws InterruptedException {
+        if (n==2500)
+        {
+            System.out.println("Part filled stack in " + Thread.currentThread().getName());
+//            Thread.sleep(30*1000);
+            return 0;
+        }
+        return halfFillStack(n+1);
+    }
+
+    static private int doNothing(int n)
+    {
+        return n;
+    }
 }
